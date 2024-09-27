d7872bd2d9ac96742c3031e7dfc0005f6a32dcf2
N'AN <sirinan9063@gmail.com>
2024-09-27 10:32:13 +0700

    Create indoor.py
    From PS0

---
+++ indoor.py
@@ -0,0 +1,8 @@
+#ask user to yell(type in all uppercase)
+answer = input("YELL AT ME! ")
+
+#Set 'answer' variable to strip empty spaces and put all letters in lowercase
+answer = answer.strip().lower()
+
+#Print the variable
+print(answer)
