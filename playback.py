6d8d2a2f3c56f3e4c658fb68b2cac5c0b7f478da
N'AN <sirinan9063@gmail.com>
2024-09-27 10:37:07 +0700

    Create playback.py
    From PS0

---
+++ playback.py
@@ -0,0 +1,8 @@
+#get user input, assign to variable "answer"
+answer = input()
+
+#assign "..." to variable "replacement"
+replacement = "..."
+
+#print user's input but replace the blank spaces with the assigned variable
+print(answer.replace(" ", replacement))
